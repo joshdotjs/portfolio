ce6a20a3a5ed080e0d8d08972f646c68fb343ff4
joshdotjs <joshdotjs@gmail.com>
2023-02-23 18:03:20 -0600

    responsive hamburger bar

--- comps/_layout/header.js
+++ comps/_layout/header.js
@@ -67,6 +67,12 @@ export default function Header() {
             </li>
           </ul>
 
+          <div id="hamburger">
+            <svg xmlns="http://www.w3.org/2000/svg" width="27.5" height="27.5" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+            </svg>
+          </div>
+
         </div>
       </header>
     </>
