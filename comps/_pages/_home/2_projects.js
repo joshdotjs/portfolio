51e8adc53bc5f7122c678ef1313e68e9d091ddc2
joshdotjs <joshdotjs@gmail.com>
2023-02-25 13:02:44 -0600

    began the project section

--- comps/_pages/_home/2_projects.js
+++ comps/_pages/_home/2_projects.js
@@ -3,15 +3,18 @@ import { Fragment } from "react";
 
 // ==============================================
 
-const Panel = ({ id, className, img }) => {
+const Panel = ({ id, className, title, description, icon, img }) => {
 
   return (
     <div id={id} className={`panel ${className}`}>
 
-
+      <div className="left">
+        <h2 className="title">{ title }</h2>
+        <p className="description">{ description }</p>
+      </div>
       
 
-      <img src={img}/>
+      <img className="right" src={img}/>
     </div>  
   );
 };
@@ -23,6 +26,13 @@ const projects = [
     id: 'project-1',
     img: '/img/code-react.png',
     title: 'Project 1',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl eu nunc. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl eu nunc.',
+    icon: '🚀'
+  },
+  {
+    id: 'project-2',
+    img: '/img/code-react.png',
+    title: 'Project 2',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl eu nunc. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl eu nunc.',
     icon: '🚀'
   }
