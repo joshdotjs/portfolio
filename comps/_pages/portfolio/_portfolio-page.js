428249c847465092ea62e3e6fa7e3f4c15681e4d
joshdotjs <joshdotjs@gmail.com>
2023-02-28 16:00:11 -0600

    portfolio page

---
+++ comps/_pages/portfolio/_portfolio-page.js
@@ -0,0 +1,13 @@
+import Projects from 'comps/_pages/_home/2_projects';
+
+// ==============================================
+
+export default function Page() {
+  return (
+    <>
+      <Projects />
+    </>
+  );
+}
+
+// ==============================================
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,11 +1,12 @@
 import Layout from "comps/_layout/_layout";
+import Page from 'comps/_pages/portfolio/_portfolio-page';
 
 // ==============================================
 
 export default function PortfolioPage() {
   return (
-    <Layout id="portfolio">
-      Porfolio page
+    <Layout id="home">
+      <Page />
     </Layout>
   );
 }
