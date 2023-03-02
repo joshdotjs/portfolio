cab14d07db445cbcccbc3ae20287c9e6da508f40
joshdotjs <joshdotjs@gmail.com>
2023-03-01 19:27:35 -0600

    mini projects rows

---
+++ data/projects-mini.js
@@ -0,0 +1,62 @@
+// ==============================================
+
+const mini_projects = [
+  {
+    img: '',
+    title: 'Parallax Landing Page',
+    title_mobile: 'Parallax',
+    description: [
+      ``,
+    ],
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="icon bi bi-cart2" viewBox="0 0 16 16">
+        <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+      </svg>
+    ),
+    logos: [
+      '/logos/react.svg',
+      '/logos/gsap.svg',
+      '/logos/sass.svg',
+    ],
+    links: {
+      code:    'https://github.com/joshdotjs/mini-demos/tree/main/00-parallax-landing-page',
+      demo:    'https://react-clone-demo.netlify.app',
+      // sandbox: '',
+    },
+    bullets: [
+      ``,
+    ],
+  },
+  {
+    img: '',
+    title: 'Accordian FAQ',
+    title_mobile: 'Accordian',
+    description: [
+      ``,
+    ],
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="icon bi bi-cart2" viewBox="0 0 16 16">
+        <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+      </svg>
+    ),
+    logos: [
+      '/logos/react.svg',
+      '/logos/gsap.svg',
+      '/logos/sass.svg',
+    ],
+    links: {
+      code:    'https://github.com/joshdotjs/mini-demos/tree/main/01-accordian-faq',
+      demo:    'https://mini-demo-accordian.netlify.app/',
+      // sandbox: '',
+    },
+    bullets: [
+      ``,
+    ],
+  },
+
+  
+];
+
+// ==============================================
+
+export default projects;
